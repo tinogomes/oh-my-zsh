24202580d01aa296b6f9a8d837822f9a64537d7e
tinogomes <tinorj@gmail.com>
2012-11-16 10:00:21 -0200

    correction: new nocorrect for su command

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -2,11 +2,13 @@ if [[ "$ENABLE_CORRECTION" == "true" ]]; then
   alias ebuild='nocorrect ebuild'
   alias gist='nocorrect gist'
   alias heroku='nocorrect heroku'
+  alias heroku='nocorrect heroku'
   alias hpodder='nocorrect hpodder'
   alias man='nocorrect man'
   alias mkdir='nocorrect mkdir'
   alias mv='nocorrect mv'
   alias mysql='nocorrect mysql'
+  alias su='nocorrect su'
   alias sudo='nocorrect sudo'
 
   setopt correct_all
