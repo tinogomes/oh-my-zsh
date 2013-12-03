484d1a431ad98936992f0cc0c3600482619b17c4
tinogomes <tinorj@gmail.com>
2013-12-03 10:36:50 -0200

    RSpec function to execure all specs step by step

--- plugins/rspec/rspec.plugin.zsh
+++ plugins/rspec/rspec.plugin.zsh
@@ -1,3 +1,27 @@
 function rspec_with () {
-  grep -l $1 spec/*/*_spec.rb | xargs rspec
+	grep -l $1 spec/*/*_spec.rb | xargs rspec
+}
+
+function rspec_all_step_by_step () {
+	echo '\n**********************************************************'
+	echo 'Migration and preparing db for tests'
+	bundle exec rake db:migrate db:test:prepare
+	
+	dirs=($(find spec -type d -depth 1 ! -name factories ! -name support ! -name fixtures ! -name javascripts ! -name tmp)) 
+	for dirname in $dirs
+	do
+		echo '\n**********************************************************'
+		echo "$dirname specs"
+		RAILS_ENV=test bundle exec rspec $dirname
+	done
+	
+	echo '\n**********************************************************'
+	echo 'javascript specs'
+	RAILS_ENV=test bundle exec rake spec:javascript
+	
+	echo '\n**********************************************************'
+	echo 'integration specs'
+	RAILS_ENV=test bundle exec rspec features/*_spec.rb
+	
+	echo
 }
