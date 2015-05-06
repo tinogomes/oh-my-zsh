ef6599d732ba2d9c9247bbeae441b6c71714c377
tinogomes <tinorj@gmail.com>
2015-05-06 18:45:46 -0300

    new plugin for azk

---
+++ plugins/azk/azk.plugin.zsh
@@ -0,0 +1,15 @@
+alias adoctor='azk doctor'
+alias ainfo='azk info'
+alias alogs='azk logs -f'
+alias areload='azk reload'
+alias arestart='azk restart'
+alias ascale='azk scale'
+alias ashell='azk shell'
+alias astart='azk start'
+alias astatus='azk status'
+alias astop='azk stop'
+
+# Azk Agent
+alias aastatus='azk agent status'
+alias aastart='azk agent start'
+alias aastop='azk agent stop'
