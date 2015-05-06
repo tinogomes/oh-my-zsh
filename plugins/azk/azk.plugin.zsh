80c2578c413631423d56f14ce7b56c7fcef6706d
tinogomes <tinorj@gmail.com>
2015-05-06 18:54:29 -0300

    replace alias ashell for a function

--- plugins/azk/azk.plugin.zsh
+++ plugins/azk/azk.plugin.zsh
@@ -4,7 +4,6 @@ alias alogs='azk logs -f'
 alias areload='azk reload'
 alias arestart='azk restart'
 alias ascale='azk scale'
-alias ashell='azk shell'
 alias astart='azk start'
 alias astatus='azk status'
 alias astop='azk stop'
@@ -13,3 +12,9 @@ alias astop='azk stop'
 alias aastatus='azk agent status'
 alias aastart='azk agent start'
 alias aastop='azk agent stop'
+
+function ashell () {
+  local system_name=$1
+  shift
+  azk shell $system_name -c "$*"
+}
